128ecbe14fd66da9bc65f56d5da9aef24dae0a61
Dtikoli <tikolidavid@yahoo.com>
2023-12-05 07:28:52 -0800

    Adding a solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists """
+
+
+def canUnlockAll(boxes):
+    """ Determines if boxes can be unlocked """
+    unlocked = {}
+
+    for index, box in enumerate(boxes):
+        if len(box) == 0 or index == 0:
+            unlocked[index] = index
+        for key in box:
+            if key < len(boxes) and key != index:
+                unlocked[key] = key
+    return len(unlocked) == len(boxes)
